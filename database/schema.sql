d024ea8da420751e91fe01bf452d778c94197cc9
xmashalx <Mashalchr@gmail.com>
2026-01-27 12:44:48 +0000

    add change to primary keys in schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,12 @@
 -- Create Country table
 CREATE TABLE Country (
-    country_id SMALLINT PRIMARY KEY,
+    country_id SMALLINT PRIMARY KEY IDENTITY(1,1),
     country VARCHAR(255)
 );
 
 -- Create Location table
 CREATE TABLE Location (
-    location_id BIGINT PRIMARY KEY,
+    location_id BIGINT PRIMARY KEY IDENTITY(1,1),
     city VARCHAR(255),
     country_id SMALLINT,
     lat FLOAT,
@@ -16,7 +16,7 @@ CREATE TABLE Location (
 
 -- Create Plant table
 CREATE TABLE Plant (
-    plant_id SMALLINT PRIMARY KEY,
+    plant_id SMALLINT PRIMARY KEY IDENTITY(1,1),
     scientific_name VARCHAR(255),
     common_name VARCHAR(255),
     location_id BIGINT,
@@ -25,7 +25,7 @@ CREATE TABLE Plant (
 
 -- Create Botanist table
 CREATE TABLE Botanist (
-    botanist_id SMALLINT PRIMARY KEY,
+    botanist_id SMALLINT PRIMARY KEY IDENTITY(1,1),
     botanist_name VARCHAR(255),
     email VARCHAR(255),
     phone VARCHAR(20)
@@ -33,7 +33,7 @@ CREATE TABLE Botanist (
 
 -- Create Record table
 CREATE TABLE Record (
-    id BIGINT PRIMARY KEY,
+    id BIGINT PRIMARY KEY IDENTITY(1,1),
     plant_id SMALLINT,
     recording_taken DATE,
     moisture FLOAT,
