1cb1f9fb9ebb6c07423254e504836842f360c3ae
xmashalx <Mashalchr@gmail.com>
2026-01-28 17:28:46 +0000

    change image_uri for api-pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "plant_load" {
   function_name = "c21-boxen-plant-load-pipeline"
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:v4"
   timeout       = 300
   memory_size   = 512
 
